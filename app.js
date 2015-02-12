1d96b1ec30a75a4a38c59d4ecd18699e57837b7c
Tylar Murray <murray.tylar@gmail.com>
2015-02-12 18:11:36 -0500

    fixed req.body issue. basic demo now working

--- app.js
+++ app.js
@@ -2,16 +2,22 @@ var express = require('express');
 var app = express();
 var serveStatic = require('serve-static');
 var fs = require('fs');
+var bodyParser = require('body-parser')
+
+// set up for post req.body parser
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+    extended: true
+}));
 
 // serve static pages
-app.use(['gameData','css', 'js', '/'], serveStatic('./', {'index': ['index.html']}));
+app.get('/*', serveStatic('./', {'index': ['index.html']}));
 
 // accept POST request for updating the game
 app.post('/uploadCurrentGame', function (req, res) {
     console.log(req.body);
 
-    /*
-    TODO: save the file (once req.body is working correctly...
+    //TODO: save the file (once req.body is working correctly...
     fs.writeFile('gameData/currentGame.json', JSON.stringify(req.body), function(err){
         if (err){
             console.log(err);
@@ -19,8 +25,7 @@ app.post('/uploadCurrentGame', function (req, res) {
             console.log('currentGame updated');
         }
     });
-    */
-
 });
 
+console.log('listening on port 8080...');
 app.listen(8080);
--- js/incrementCounter.js
+++ js/incrementCounter.js
@@ -2,11 +2,11 @@ function incrementCounter(){
     // gets the raw game save json increments the counter, POSTS the data
     $.get( "gameData/currentGame.json", function( data ) {
         window.currentGame = data;
-        window.currentGame["counter"] += 1;
+        window.currentGame["counter"] = parseInt(window.currentGame["counter"]) + 1;
 
         $.post( "/uploadCurrentGame", window.currentGame, function( response ) {
             alert('new game json uploaded');
-            res.send('success');
+            response.send('success');
         });
 
     });
