9dd5705df553d06d616a4d2e0f7c4d2a581b20c8
7yl4r <murray.tylar@gmail.com>
2015-02-15 20:23:10 -0500

    added playerSelector & basic players to data structure

---
+++ js/controls.js
@@ -0,0 +1,41 @@
+// this script is for populating and responding to elements on the main control page
+
+// =============================
+// === === === utils === === ===
+// =============================
+getPlayerData = function(playerName){
+    // returns player data object for given player name
+    for (var i = 0; i < window.gameData.players.length; i++) {
+        if( window.gameData.players[i].name == playerName ){
+            return window.gameData.players[i];
+        }
+    }
+    // if name not found
+    throw new Error("player " + playerName + "not found");
+}
+// =============================
+
+
+// ================================
+// === choose-a-player dropdown ===
+// ================================
+$(document).on("gameDataLoaded", function () {
+    var listItems = '<option selected="selected" value="0">- Select -</option>';
+
+    for (var i = 0; i < window.gameData.players.length; i++) {
+         listItems += "<option value='" + window.gameData.players[i].name + "'>"
+            + window.gameData.players[i].name
+            + "</option>";
+    }
+
+    $("#player-selector").html(listItems);
+});
+
+$("#player-selector").on("change", function(evt){
+    var player = $("#player-selector").val();
+    console.log(player);
+
+    playerData = getPlayerData(player);
+});
+
+// ================================
--- js/showRawSave.js
+++ js/showRawSave.js
@@ -2,6 +2,8 @@ function showRawSave(selectorStr){
     // gets the raw game save json and inserts it into elements retrieved via given selector string
     $.get( "gameData/currentGame.json", function( data ) {
         $( selectorStr ).html( JSON.stringify(data));
+        window.gameData = data;
+        $(document).trigger("gameDataLoaded");
     });
 }
 
